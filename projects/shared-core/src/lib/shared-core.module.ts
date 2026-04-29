4854c326d7d3704152197f7b34c8763a295cefcd
Jie Li <jl@ocg.de>
2026-04-29 16:51:33 +0200

    added angular material

--- projects/shared-core/src/lib/shared-core.module.ts
+++ projects/shared-core/src/lib/shared-core.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+
 import { TitleBarComponent } from './components/title-bar/title-bar.component';
 
 @NgModule({
     declarations: [TitleBarComponent],
+    imports: [MatCardModule],
     exports: [TitleBarComponent],
 })
 export class SharedCoreModule { }
